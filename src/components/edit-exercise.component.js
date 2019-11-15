33fcffa0cfdd3b8d5838c7f50d0ee042c5438558
mikergarrity <mikergarrity@gmail.com>
2019-11-15 08:57:49 -0600

    initial commit, finished setting up backend, starting frontend

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the Edit Exercise component</p>
+      </div>
+    );
+  }
+}
